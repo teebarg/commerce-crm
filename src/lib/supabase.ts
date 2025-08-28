d0d4f918a2a2b059addc5a6cc6b5e94c1a5925eb
Adeniyi <teebarg01@gmail.com>
2025-08-28 11:14:46 +0100

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { env } from "@/env";
 
 export const supabase = createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
-export async function uploadMediaToSupabase(file: File, folder = "media") {
+export async function uploadMediaToSupabase(file: File, folder = "images") {
     const fileExt = file.name.split(".").pop();
     const fileName = `${folder}/${Date.now()}-${Math.random().toString(36).substring(2, 8)}.${fileExt}`;
     const { error } = await supabase.storage.from("media").upload(fileName, file, {
