f9b974386f57e323416138bf9a8af401c974c9ee
Adeniyi <teebarg01@gmail.com>
2025-09-05 18:48:54 +0100

    Fix redis group

--- src/app/api/worker/route.ts
+++ src/app/api/worker/route.ts
@@ -2,6 +2,10 @@ import { type NextRequest, NextResponse } from "next/server";
 import { redis } from "@/lib/redis";
 import { db } from "@/server/db";
 
+const STREAM_NAME = "events:USER_REGISTERED";
+const GROUP_NAME = "order-group";
+const CONSUMER_NAME = "consumer-1";
+
 export async function POST(request: NextRequest) {
     try {
         if (!redis) {
@@ -10,7 +14,15 @@ export async function POST(request: NextRequest) {
 
         const { limit = 50 } = await request.json();
 
-        const events = await redis.xrange("events:USER_REGISTERED", "-", "+", "COUNT", limit);
+        // const events = await redis.xrange("events:USER_REGISTERED", "-", "+", "COUNT", limit);
+        // const events = await redis.xreadgroup("GROUP", GROUP_NAME, CONSUMER_NAME, [[STREAM_NAME, ">"]], {
+        //     COUNT: limit,
+        //     BLOCK: 5000,
+        // });
+
+        // XREADGROUP GROUP group consumer [COUNT count] [BLOCK milliseconds] STREAMS key [key ...] ID [ID ...]
+        const events = await redis.xreadgroup("GROUP", GROUP_NAME, CONSUMER_NAME, "COUNT", limit, "BLOCK", 5000, "STREAMS", STREAM_NAME, ">");
+        console.log("🚀 ~ file: route.ts:25 ~ events:", events)
 
         if (!events.length) {
             return NextResponse.json({
@@ -22,18 +34,20 @@ export async function POST(request: NextRequest) {
         let processedCount = 0;
         const errors: string[] = [];
 
-        for (const [id, fields] of events) {
-            const data = JSON.parse(fields[1]!);
-            try {
-                await processNewUserEmail(data);
-                await redis.xdel("events:USER_REGISTERED", id);
-                processedCount++;
-            } catch (error) {
-                const errorMsg = `Failed to process event: ${error instanceof Error ? error.message : "Unknown error"}`;
-                errors.push(errorMsg);
-                console.error(errorMsg, id);
-            }
-        }
+        // for (const [id, fields] of events) {
+        //     const data = JSON.parse(fields[1]!);
+        //     try {
+        //         await processNewUserEmail(data);
+        //         // acknowledge the event
+        //         await redis.xack(STREAM_NAME, id);
+        //         // await redis.xdel("events:USER_REGISTERED", id);
+        //         processedCount++;
+        //     } catch (error) {
+        //         const errorMsg = `Failed to process event: ${error instanceof Error ? error.message : "Unknown error"}`;
+        //         errors.push(errorMsg);
+        //         console.error(errorMsg, id);
+        //     }
+        // }
 
         return NextResponse.json({
             message: "Events processed",
@@ -48,7 +62,7 @@ export async function POST(request: NextRequest) {
 
 async function processNewUserEmail(data: { email: string; first_name: string; last_name: string; group?: string }) {
     const { email, first_name, last_name, group } = data;
-    const name = first_name + last_name
+    const name = first_name + last_name;
 
     const contact = await db.emailContact.upsert({
         where: { email },
